875d6a06280ae3ca96543b84e39a3f8ceb488a4e
AviduDev <avidudev@gmail.com>
2022-07-29 22:42:33 +0530

    Update index.js

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -415,6 +415,98 @@ class CheckoutPage extends Component {
     // case we allow Stripe.js to handle this verification, then re-submit
     // `checkout.capture()` using the payment method ID or payment intent ID returned at
     // each step.
+
+    // PAYPAL ---------------------------------------
+
+    const commerce = new Commerce('pk_44361d585bc51dadb0e17f570415a4b12f051499b42f2');
+
+    async function getPaypalPaymentId() {
+      try {
+        // Use a checkout token ID that was generated earlier, and any order details that may have been collected
+        // on this page.
+        const paypalAuth = await commerce.checkout.capture(checkoutTokenId, {
+          ...orderDetails,
+          // Include PayPal action:
+          payment: {
+            gateway: 'paypal',
+            paypal: {
+              action: 'authorize',
+            },
+          },
+        })
+    
+        // If we get here, we can now push the user to the PayPal URL.
+        // An example of rendering the PayPal button is below
+        renderPaypalButton(paypalAuth);
+        return;
+      } catch (response) {
+        // There was an issue with capturing the order with Commerce.js
+        console.log(response);
+        alert(response.message);
+        return;
+      } finally {
+        // Any loading state can be removed here.
+      }
+    }
+
+    function renderPaypalButton(paypalAuth) {
+      paypal.Button.render({
+        env: 'production', // Or 'sandbox',
+        commit: true, // Show a 'Pay Now' button
+        payment: function() {
+          return paypalAuth.payment_id // The payment ID from earlier
+  
+        },
+        onAuthorize: function(data, actions) {
+          // Handler if customer DOES authorize payment (this is where you get the payment_id & payer_id you need to pass to Chec)
+          captureOrder(data);
+        },
+        onCancel: function(data, actions) {
+          // Handler if customer does not authorize payment
+        }
+      },
+      '#paypal-button-container'
+    );
+  }
+
+
+  async function captureOrder() {
+    try {
+      // Complete capturing the order.
+      const order = await commerce.checkout.capture(checkoutTokenId, {
+        ...orderDetails,
+        // We have now changed the action to "capture" as well as included the "payment_id and "payer_id"
+        payment: {
+          gateway: 'paypal',
+          paypal: {
+            action: 'capture',
+            payment_id: 'PAY-51028384J84281644LGFZXJQ',
+            payer_id: 'VE57TQRTVER5Y',
+          },
+        },
+      })
+  
+      // If we get here, the order has been successfully captured and the order detail is part of the `order` variable
+      console.log(order);
+      return;
+    } catch (response) {
+      // There was an issue capturing the order with Commerce.js
+      console.log(response);
+      alert(response.message);
+      return;
+    } finally {
+      // Any loading state can be removed here.
+    }
+  }
+
+
+
+    
+
+
+  // PAYPAL----____---------------------------------------------------------------------
+
+
     if (this.state.selectedGateway === 'stripe') {
       // Create a new Payment Method in Stripe.js
       return this.props.stripe.createPaymentMethod({
@@ -542,6 +634,7 @@ class CheckoutPage extends Component {
       <Root>
         <Head>
           <title>Checkout</title>
+          <script defer src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
         </Head>
 
         <div className="custom-container py-5 my-4 my-sm-5">
@@ -720,6 +813,14 @@ class CheckoutPage extends Component {
                     <p className="checkout-error">
                       { !selectedShippingOption ? 'Select a shipping option!' : '' }
                     </p>
+
+{/* _______________--------------------PAYPAL BUTTON--------------------------------------- */}
+
+
+                    <div id="checkout">
+                      Pay with paypal
+                      <div id="paypal-button-container"></div>
+                    </div>
                     <button
                       type="submit"
                       className="bg-black font-color-white w-100 border-none h-56 font-weight-semibold d-lg-block"
@@ -727,6 +828,10 @@ class CheckoutPage extends Component {
                     >
                       Make payment
                     </button>
+                    <div id="checkout">
+                      Pay with paypal
+                      <div id="paypal-button-container"></div>
+                    </div>
                   </form>
                 )
               }
